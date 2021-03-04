5780a5815b173a22b3c889f711072e84a3d33374
Francesco Del Pozzo <fradelpo@yahoo.it>
2021-03-04 15:39:31 +0100

    Added functions and strings

--- app.js
+++ app.js
@@ -4,17 +4,20 @@ var myApp = angular.module('myApp', []);
 // CONTROLLERS
 myApp.controller('mainController', ['$scope', function ($scope) {
     
-    $scope.name = "Tony";
-    $scope.occupation = "Coder";
+    // $scope.name = "Tony";
+    // $scope.occupation = "Coder";
     
-    $scope.getname = function() {
-        return "John Doe";
-    }
+    // $scope.getname = function() {
+    //     return "John Doe";
+    // }
 
-    // $scope.getname();
+    // // $scope.getname();
 
-    console.log($scope.getname());
+    // console.log($scope.getname());
 }]);
 
+var searchPeople = function(firstName, lastName, height, age, occupation) {
+    return "Jane Doe";
+}
 
-
+console.log(searchPeople(1, 2, 3, 4, 5));
